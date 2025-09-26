c17e9ceb1e8d9094843cdb7483b2c3d12b0e3630
Xavier Fiechter <xavierfiechter@gmail.com>
2025-09-26 23:24:07 +0200

    version bump to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bip329',
-    version='0.1.0-pre2',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'py7zr',
